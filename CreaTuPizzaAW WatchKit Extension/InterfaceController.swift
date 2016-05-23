f800e898c8718f4e9c62eb0731007e70330939f7
xcaretac <xcaretac@gmail.com>
2016-05-23 02:42:24 -0500

    Initial Commit

---
+++ CreaTuPizzaAW WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  CreaTuPizzaAW WatchKit Extension
+//
+//  Created by Xcaret A Ceniceros on 23/05/16.
+//  Copyright © 2016 Xcaret Arellano Ceniceros. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
